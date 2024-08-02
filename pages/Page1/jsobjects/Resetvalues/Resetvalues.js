2c33f0e86763362f6d5c6d17bbcf50bf5693c007
Ian Relosa <ianrelosa6@gmail.com>
2024-08-02 19:51:36 +0000

    Added modal for insert

---
+++ pages/Page1/jsobjects/Resetvalues/Resetvalues.js
@@ -0,0 +1,10 @@
+export default {
+	Button6onClick () {
+ m_title.setValue('');
+m_vote.setValue('');
+		m_votecount.setValue('');
+		m_status.setSelectedOption('');
+		m_genres.setSelectedOptions('');
+		m_releasedate.setValue('');
+	}
+}
